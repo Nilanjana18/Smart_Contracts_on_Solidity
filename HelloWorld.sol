0ea7c8b0b9db5541dd38cea02f6edb72a14910de
Nilanjana Roy <nilanjanaroy524@gmail.com>
2023-02-18 14:03:36 +0530

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >= 0.5.0 < 0.9.0;
+contract HelloWorld {
+ string private helloMessage = "Hello world";
+ function getHelloMessage() public view returns (string memory) {
+ return helloMessage;
+ }
+}
